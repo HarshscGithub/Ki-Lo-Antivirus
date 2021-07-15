02e3ac0069ff41ae6420a80bc7c1d26cdcf079c8
harshsc2007 <74180708+harshsc2007@users.noreply.github.com>
2021-07-15 15:38:30 +0530

    Add files via upload
    Ki-Lo Antivirus. Major version of Dashing Antivirus.
    Version 0.41.2
    UI is completely changed.
    Actually it should be v2.0 but all work is not completed
    
    Update and Info/About pages are still in progress

---
+++ Kilo_Antivirus/path_icon.py
@@ -0,0 +1,4 @@
+import os
+  
+icon_path = os.path.abspath("Desktop/Kilo_Antivirus/icon.png")
+
---
+++ Kilo_Antivirus/path_no_vf.py
@@ -0,0 +1,5 @@
+import os
+  
+Picture_path = os.path.abspath("Desktop/Kilo_Antivirus/No_Virus_Found.png")
+
+print(Picture_path)
